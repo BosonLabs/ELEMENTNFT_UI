466d84bc6b84db453b86c2fe8609bb2f00d6a27b
ramBoson <2020ramboson@gmail.com>
2022-01-25 14:15:44 +0530

    other fixed liked

--- src/components/Snippets/tabs/OnSaleOther.js
+++ src/components/Snippets/tabs/OnSaleOther.js
@@ -268,7 +268,7 @@ const OnSaleOther = (data) => {
                     <>                    
                     <div className='col mb-4' >
                     <CardBuyOtherView img={x.Imageurl} 
-                    title={x.NFTName} count="401" subTitle={`<span>Highest bid</span> <span>1/1</span>`} 
+                    title={x.NFTName} count="401" subTitle={x.AssetId} 
                     linkText={parseInt(x.NFTPrice/1000000)} dataall={x}
                     />
                     </div>                    
