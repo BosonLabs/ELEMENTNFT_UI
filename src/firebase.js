c42c21bc9af5ea656077343ddcdcdf4d6bcf2c53
ramBoson <2020ramboson@gmail.com>
2022-03-18 10:11:34 +0530

    apikeyanddburl only visible

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import config from "./config.json"
 // };
 const firebaseConfig = {
   apiKey: "AIzaSyAZBlbVDfnV7CKTtEk_EUjwXYtEG11wMiE",
-  authDomain: "nftmarketplace-7bcb5.firebaseapp.com",
+  //authDomain: "nftmarketplace-7bcb5.firebaseapp.com",
   databaseURL: "https://nftmarketplace-7bcb5-default-rtdb.firebaseio.com",
-  projectId: "nftmarketplace-7bcb5",
-  storageBucket: "nftmarketplace-7bcb5.appspot.com",
-  messagingSenderId: "814546102167",
-  appId: "1:814546102167:web:ae135dc0058e34dff806d2",
-  measurementId: "G-Z6Z5933K60"
+  //projectId: "nftmarketplace-7bcb5",
+  //storageBucket: "nftmarketplace-7bcb5.appspot.com",
+  //messagingSenderId: "814546102167",
+  //appId: "1:814546102167:web:ae135dc0058e34dff806d2",
+  //measurementId: "G-Z6Z5933K60"
 };
 
 const fireDb = firebase.initializeApp(firebaseConfig);
